4fa23d14dbeaaa6a2838b39f3c5c97e94e3122ba
StartForKillerMC <jesussanz2003@gmail.com>
2019-05-25 17:38:41 +0200

    Changed to Klasa

--- bot.js
+++ bot.js
@@ -6,5 +6,9 @@ new Client({
     commandEditing: true,
     typing: true,
     providers: { default: 'mongodb' },
+    gateways: {
+        guilds: { provider: 'mongodb' },
+        users: { provider: 'mongodb' }
+    },
     readyMessage: (client) => `${client.user.tag}, Ready to serve ${client.guilds.size} guilds and ${client.users.size} users`
 }).login(process.env.BOT_TOKEN);
